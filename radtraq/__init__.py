ffa2159eaf74d8b7c4e7fa93949c69d6da147466
Adam Theisen <atheisen@anl.gov>
2019-09-15 20:08:05 -0500

    ADD:  Adding noise floor calculation

--- radtraq/__init__.py
+++ radtraq/__init__.py
@@ -11,6 +11,8 @@ This contains code for radar quality assurance and tracking
     :toctree: generated/
 
     cfad
+    noise
 """
 
 from . import cfad
+from . import noise
--- radtraq/cfad.py
+++ radtraq/cfad.py
@@ -1,30 +1,81 @@
+"""
+radtraq.cfad
+------------
+
+Module for calculating and plotting a cfad using 
+an act object as input
+
+"""
+
+import dask
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def plot_cfad(hist, x, y):
+    """
+    Function for plotting up CFAD given 2D histogram, x, and y
+
+    Parameters
+    ----------
+    hist : list
+        2D list of histogram data from calc_cfad
+    x : list
+        List of x values
+    y : list 
+        List of y values
+
+    Returns
+    -------
+    fig : matplotlib ax handle
+        Returns the axis handle for additional updates if needed
+
+    """
 
     fig, ax = plt.subplots()
     cs = ax.contourf(x, y, hist, 50)
     cbar = fig.colorbar(cs)
-    plt.show()
-    
 
-def calc_cfad(obj, variable, hvariable, xbins=None, log=True, yint=20):
-    #Currently assuming vertical point data from netcdf files
+    return ax
+
+def calc_cfad(obj, variable, hvariable, xbins=None):
+    """
+    Function for plotting up CFAD given 2D histogram, x, and y
+
+    Parameters
+    ----------
+    object : xarray Dataset
+        ACT object containing vertical point data
+    variable : string
+        Variable to calculate CFAD for
+    hvariable : string 
+        Variable name for the height data
+    xbins : list 
+        List of bins to calculate cfad for
+
+    Returns
+    -------
+    hist : list
+        2D List of histogram results
+    xbins : list
+        List of xbins that were used
+    height : list
+        List of heights from the hvariable
+
+    """
     data = obj[variable]
     height = obj[hvariable]
 
     if xbins is None:
         xbins = np.linspace(-70, 50, 121)
-    print(xbins)
 
-    hist = []
-    for j, ht in enumerate(height):
-        h, bin_edge =  np.histogram(data[:,j:j+yint], bins=xbins)
-        if log is True:
-            h = np.log10(h)
-        hist.append(list(h))
-        j += yint
+    dsk = []
+    for j in range(len(height)):
+        a = dask.delayed(np.histogram)(data[:,j], bins=xbins)
+        dsk.append(a)
+
+    hist = dask.compute(*dsk)
+
+    hist = [np.log10(h[0]) for h in hist]
 
     return hist, xbins[:-1], height
