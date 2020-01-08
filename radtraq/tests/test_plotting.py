99f5c2a5c253df3578a1eb72c28b196b2197e60d
Adam Theisen <atheisen@anl.gov>
2020-01-08 14:37:28 -0600

    ENH: Updates

--- radtraq/tests/test_plotting.py
+++ radtraq/tests/test_plotting.py
@@ -1,12 +1,14 @@
 import radtraq
 import pytest
 import act
+import numpy
 
 @pytest.mark.mpl_image_compare(tolerance=10)
 def test_plotting():
     f = radtraq.tests.sample_files.EXAMPLE_KAZR
     obj = act.io.armfiles.read_netcdf(f)
 
+    numpy.seterr(divide = 'ignore') 
     hist, x, y = radtraq.plotting.cfad.calc_cfad(obj,'reflectivity_copol', 'range')
     display = radtraq.plotting.cfad.plot_cfad(hist, x, y)
-    return display.fig
+    return display.figure
