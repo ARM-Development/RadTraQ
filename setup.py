a5bfeb8e8c506643bf0a9cc64739e16a0f7eaa9d
Adam Theisen <atheisen@anl.gov>
2020-10-25 13:46:54 -0500

    WIP

--- setup.py
+++ setup.py
@@ -8,17 +8,6 @@ import sys
 # and/or pip.
 min_version = (3, 6)
 if sys.version_info < min_version:
-    error = """
-act does not support Python {0}.{1}.
-Python {2}.{3} and above is required. Check your Python version like so:
-
-python3 --version
-
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
-
-pip install --upgrade pip
-""".format(*sys.version_info[:2], *min_version)
     sys.exit(error)
 
 here = path.abspath(path.dirname(__file__))
