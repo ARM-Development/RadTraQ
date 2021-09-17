e8a9c60ff1444d59613a3216cfa30c9e8fd04538
Kenneth Kehoe <kkehoe@ou.edu>
2021-09-17 11:17:31 -0600

    Cleaned up documentation and made cloud_threshold() function used in Dask a bit better.

--- radtraq/proc/noise.py
+++ radtraq/proc/noise.py
@@ -9,7 +9,7 @@ mask to the data
 
 import dask
 import numpy as np
-import warnings
+import xarray as xr
 
 from ..utils.corrections import range_correction
 
@@ -35,75 +35,84 @@ def calc_noise_floor(obj, variable, height_variable=None):
 
     """
 
+    if not isinstance(obj, xr.core.dataset.Dataset):
+        raise ValueError('Please use a valid Xarray.Dataset')
+
+    if not isinstance(variable, str):
+        raise ValueError('Please Specify a Variable Name')
+
+    # Range correct data and return the DataArray from Dataset
     data = range_correction(obj, variable, height_variable=height_variable)
-    n_t, n_h = np.shape(data)
 
-    task = []
-    for i in range(n_t):
-        task.append(dask.delayed(cloud_threshold)(data[i, :], 1, n_h))
+    # Pass each timestep into task list to calculate cloud threshhold
+    # with a delayed dask process
+    task = [dask.delayed(cloud_threshold)(row) for row in data]
 
+    # Perform dask computation
     result = dask.compute(*task)
+
+    # Convert returned dask tuple into numpy array
     result = np.array(result, dtype=float)
 
     return result
 
 
-def cloud_threshold(data, n_avg, nffts):
+def cloud_threshold(data, n_avg=1., nffts=None):
     """
     Calculates the noise floor based on code provided to the ARM DQ Office
     by Ieng Jo and Pavlos Kollias while at McGill University
 
     Parameters
     ----------
-    data : xarray DataArray
-        xarray data array
+    data : Xarray.DataArray
+        Xarray DataArray
     n_avg : float
-        Number of points to average over.  Default is normall 1
+        Number of points to average over
     nffts : int
-        Number of heights to iterate over
+        Number of heights to iterate over. If None will use the size of data.
 
     Returns
     -------
-    result : list
-        Returns the noise floor values for each time sample
+    result : numpy scalar float
+        Returns the noise floor value for each time sample
 
     """
-    data = 10. ** (data / 10.)
+
+    if nffts is None:
+        nffts = data.size
+
+    data = 10. ** (data.values / 10.)
     data = np.sort(data)
 
     nthld = 10. ** -10.
     dsum = 0.
     sumSq = 0.
     n = 0.
+    numNs = []
+    sqrt_n_avg = np.sqrt(n_avg)
     for i in range(nffts):
         if data[i] > nthld:
             dsum += data[i]
             sumSq += data[i] ** 2.0
-            n += 1
+            n += 1.
             a3 = dsum * dsum
-            a1 = np.sqrt(n_avg) * (n * sumSq - a3)
+            a1 = sqrt_n_avg * (n * sumSq - a3)
             if n > nffts / 4.:
                 if a1 <= a3:
                     sumNs = dsum
                     numNs = [n]
-#                    maxNs = data[i]
             else:
                 sumNs = dsum
                 numNs = [n]
-#                maxNs = data[i]
 
     if len(numNs) > 0:
         n_mean = sumNs / numNs[0]
-#        n_max = maxNs
-#        n_points = numNs[0]
     else:
         n_mean = np.nan
-#        n_max = np.nan
-#        n_points = np.nan
 
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=RuntimeWarning,
-                                message='.*divide by zero encountered.*')
+    if n_mean == 0.:
+        value = np.nan
+    else:
         value = 10. * np.log10(n_mean)
 
     return value
