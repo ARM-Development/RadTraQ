b4408be7d901d9d10ae20d3233167ee7acdf23d9
Kenneth Kehoe <kkehoe@ou.edu>
2021-09-17 09:48:04 -0600

    Updated to work with changes to other functions.

--- examples/plot_cloud_mask.py
+++ examples/plot_cloud_mask.py
@@ -7,25 +7,25 @@ This example shows how to calculate a cloud mask and plot data
 """
 
 
-import radtraq
+from radtraq.tests.sample_files import EXAMPLE_KAZR
+from radtraq.proc.cloud_mask import calc_cloud_mask
 import act
 import matplotlib.pyplot as plt
 
 # Read in sample data using ACT
-f = radtraq.tests.sample_files.EXAMPLE_KAZR
-obj = act.io.armfiles.read_netcdf(f)
+obj = act.io.armfiles.read_netcdf(EXAMPLE_KAZR)
 
 # Resample data for ease of processing
-ge = obj.resample(time='1min').nearest()
+obj = obj.resample(time='1min').nearest()
 
 # Calculate and apply cloud mask
-ge = radtraq.proc.cloud_mask.calc_cloud_mask(ge, 'reflectivity_copol', 'range')
-ge = ge.where(ge['mask2'] == 1)
+obj = calc_cloud_mask(obj, 'reflectivity_copol')
+obj = obj.where(obj['cloud_mask_2'] == 1)
 
 # Plot data using ACT
-display = act.plotting.TimeSeriesDisplay(ge)
+display = act.plotting.TimeSeriesDisplay(obj)
 display.plot('reflectivity_copol', cmap='jet')
 display.axes[0].set_ylim([0, 20000])
 plt.show()
 
-ge.close()
+obj.close()
