d95de5194abfe2f45b8562372b31ab1696030330
Adam Theisen <atheisen@anl.gov>
2021-09-17 15:47:58 -0500

    ENH: Reverting changes

--- examples/plot_cfad.py
+++ examples/plot_cfad.py
@@ -6,17 +6,15 @@ This example shows how to calculate and plot a cfad
 
 """
 
-
-from radtraq.plotting.cfad import calc_cfad, plot_cfad
-from radtraq.tests.sample_files import EXAMPLE_KAZR
+import radtraq
 from act.io.armfiles import read_netcdf
 import matplotlib.pyplot as plt
 
 # Read in example data
-obj = read_netcdf(EXAMPLE_KAZR)
+obj = read_netcdf(radtraq.tests.sample_files.EXAMPLE_KAZR)
 
 # Calculate CFAD histogram
-data_array = calc_cfad(obj, 'reflectivity_copol')
+data_array = radtraq.plotting.cfad.calc_cfad(obj, 'reflectivity_copol')
 
 # Plot CFAD histogram
 dims = data_array.dims
