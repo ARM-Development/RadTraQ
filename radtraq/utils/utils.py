79521d7f06d227d377d9294d88f2358f728f4e49
Kenneth Kehoe <kkehoe@ou.edu>
2021-09-27 15:47:13 -0600

    Made calculations in calc_ground_range_and_height() be performed in float64. Added calculate_azimuth_distance_from_lat_lon() to determine azimuth and distance between two lat/lon locations.

--- radtraq/utils/utils.py
+++ radtraq/utils/utils.py
@@ -26,12 +26,12 @@ def calc_ground_range_and_height(slant_range, elevation):
     range_units = slant_range.attrs['units']
     # Convert units to desired_units
     unit_registry = pint.UnitRegistry()
-    range_dist_values = slant_range.values
+    range_dist_values = slant_range.values.astype(np.float64)
     range_dist_values = range_dist_values * unit_registry.parse_expression(range_units)
     range_dist_values = range_dist_values.to(desired_units).magnitude
 
     # Calculate height values
-    earth_radius = 4. / 3. * 6374.  # Effective earth radius in km.
+    earth_radius = np.array(4. / 3. * 6374., dtype=np.float64)  # Effective earth radius in km.
     height = np.sqrt(earth_radius**2.0 +
                      range_dist_values**2.0 -
                      2.0 * earth_radius * range_dist_values * np.cos(np.deg2rad(elevation + 90.)))
@@ -60,3 +60,58 @@ def calc_ground_range_and_height(slant_range, elevation):
                                                                 'units': range_units, 'standard_name': 'height'})
 
     return return_dataset
+
+
+def calculate_azimuth_distance_from_lat_lon(curr_lat=None, curr_lon=None, target_lat=None,
+                                            target_lon=None):
+    """
+    Returns dictionary of distance and direction between two pairs of lat/lon values.
+
+    ...
+
+    Parameters
+    ----------
+    curr_lat : float
+        The latitude of first location assumed to be in same units as target_lat
+    curr_lon : float
+        The longitude of first location assumed to be in same units as target_lon
+    target_lat : float
+        The latitude of second location assumed to be in same units as curr_lat
+    target_lon : float
+        The longitude of second location assumed to be in same units as curr_lon
+
+    Returns
+    -------
+    dict
+        Dictionary containing azimuth and distance from first location to second location.
+        Azimiuth values are in degrees and distance is in meters.
+
+    """
+
+    azimuth = np.nan
+    distance = np.nan
+    doc = {'azimuth': azimuth, 'distance': distance}
+
+    earth_r = 6353000.0
+    lon1 = -1.0 * np.radians(curr_lon, dtype=np.float64)
+    lat1 = np.radians(curr_lat, dtype=np.float64)
+    lon2 = -1.0 * np.radians(target_lon, dtype=np.float64)
+    lat2 = np.radians(target_lat, dtype=np.float64)
+    delta_lon = lon1 - lon2
+
+    # Calcualte distance
+    y = np.sqrt((np.cos(lat2) * np.sin(delta_lon))**2 +
+                (np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(delta_lon))**2)
+    x = np.sin(lat1) * np.sin(lat2) + np.cos(lat1) * np.cos(lat2) * np.cos(delta_lon)
+
+    dist_angle = np.arctan2(y, x)
+    doc['distance'] = earth_r * dist_angle
+
+    # Calculate azimuth
+    y = np.sin(delta_lon) * np.cos(lat2)
+    x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(delta_lon)
+
+    azimuth = np.degrees(np.arctan2(y, x))
+    doc['azimuth'] = np.mod(azimuth, 360.0)
+
+    return doc
