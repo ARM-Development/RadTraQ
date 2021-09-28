47a9b6499d22cfb7ef448cbc87b5391634227d0f
Adam Theisen <atheisen@anl.gov>
2021-09-28 10:49:28 -0500

    ENH: Updating documentation

--- radtraq/utils/__init__.py
+++ radtraq/utils/__init__.py
@@ -10,8 +10,13 @@ This module contains various utilities
 .. autosummary::
     :toctree: generated/
 
-    range_correction
     calc_ground_range_and_height
+    calculate_azimuth_distance_from_lat_lon
+    get_height_variable_name
+    range_correction
 """
 
-# from .corrections import range_correction
+from .corrections import range_correction
+from .dataset_utils import get_height_variable_name
+from .utils import calc_ground_range_and_height
+from .utils import calculate_azimuth_distance_from_lat_lon
