4ba9b46515e02ef29923e4ea31586e343a854038
Kenneth Kehoe <kkehoe@ou.edu>
2021-11-12 15:16:27 -0700

    Updated to subset the data before making calculations. Also, has more keyword options to allow not knowing the distance.

--- radtraq/plotting/corner_reflector.py
+++ radtraq/plotting/corner_reflector.py
@@ -2,26 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 import pandas as pd
+import copy
 
 
 def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=None,
-                   delta_x=None, delta_y=None, az_limits=None, el_limits=None,
-                   vmin=None, vmax=None, cmap=None, title=None, title_flag=True,
-                   axislabels=[None, None], axislabels_flag=True,
-                   colorbar_flag=True, colorbar_label=None,
-                   colorbar_orient='vertical', noplot=True):
+                   delta_x=None, delta_y=None, target_limits=None, az_limits=None,
+                   el_limits=None, vmin=None, vmax=None, cmap=None, title=None,
+                   title_flag=True, axislabels=[None, None], axislabels_flag=True,
+                   colorbar_flag=True, colorbar_label=None, colorbar_orient='vertical',
+                   noplot=False, az_field='azimuth', el_field='elevation', range_field='range'):
     """
     Plot a corner reflector raster scan
 
     Parameters
     ----------
+    obj : Xarray.Dataset
+        Xarray Dataset containing data
     field : String
         Field to plot if other than reflectivity
     target_range : Float
         Estimated range of the corner reflector
-
-    Other Parameters
-    ----------------
     ax : Axis
         Axis to plot on. None will use the current axis.
     fig : Figure
@@ -30,14 +30,17 @@ def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=No
         Azimuth grid spacing for griddata
     delta_y : Float
         Elevation grid spacing for griddata
+    target_limits : list
+        Distance limits in form [min, max] used to subset the data when determining
+        what range to analyze. This is ignored if target_range is set.
     az_limits : list
         Azimuth limits in form [min, max]
     el_limits : list
         Elevation limits in form [min, max]
     vmin : float
-        Luminance minimum value, None for default value.
+        Luminance minimum value used in plotting, None for default value.
     vmax : float
-        Luminance maximum value, None for default value.
+        Luminance maximum value used in plotting, None for default value.
     cmap : str or None
         Matplotlib colormap name. None will use the default colormap for
         the field being plotted as specified by the Py-ART configuration.
@@ -46,7 +49,7 @@ def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=No
         the field and sweep parameters. Parameter is ignored if title_flag
         is False.
     title_flag : bool
-        True to add a title to the plot, False does not add a title.
+        Set to True to add a title to the plot, False does not add a title.
     axislabels : (str, str)
         2-tuple of x-axis, y-axis labels. None for either label will use
         the default axis label. Parameter is ignored if axislabels_flag is
@@ -59,38 +62,61 @@ def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=No
     colorbar_label : str
         Colorbar label, None will use a default label generated from the
         field information.
-    ticks : array
-        Colorbar custom tick label locations.
-    ticklabs : array
-        Colorbar custom tick labels.
     colorbar_orient : 'vertical' or 'horizontal'
         Colorbar orientation.
+    noplot : boolean
+        Switch to indicate if a plot should be created or just return the values.
 
+    Returns
+    -------
+    dict : dictionary
+        A dictionary with values for
+        max = Maximum data value
+        min = Maximum data value
+        az_max = Azimuth value for maximum data value, hopefully in center of corner reflector
+        el_max = Elevation value for maximum data value, hopefully in center of corner reflector
+        el_top = Elevation value for top of corner reflector
+        range = Distance to target
     """
 
     if fig is None and ax is None and noplot is False:
         fig, ax = plt.subplots()
 
     # Get data and coordinate information
-    az = obj['azimuth'].values
-    el = obj['elevation'].values
-    rng = obj['range'].values
+    az = obj[az_field].values
+    el = obj[el_field].values
+    rng = obj[range_field].values
     data = obj[field].values
 
-    # Calculate delta for x and y
-    if az_limits is None:
-        min_az = np.nanmin(az)
-        max_az = np.nanmax(az)
-    else:
-        min_az = az_limits[0]
-        max_az = az_limits[1]
+    if az_limits is not None or el_limits is not None:
+        if az_limits is not None:
+            az_limits.sort()
+            az_index = (az >= az_limits[0]) & (az <= az_limits[1])
+        else:
+            az_index = np.full(az.size, True)
 
-    if el_limits is None:
-        min_el = np.nanmin(el)
-        max_el = np.nanmax(el)
-    else:
-        min_el = el_limits[0]
-        max_el = el_limits[1]
+        if el_limits is not None:
+            el_limits.sort()
+            el_index = (el >= el_limits[0]) & (el <= el_limits[1])
+        else:
+            el_index = np.full(el.size, True)
+
+        index = az_index & el_index
+        az = az[index]
+        el = el[index]
+        data = data[index, :]
+
+    if target_limits is not None:
+        target_limits.sort()
+        index = (rng >= target_limits[0]) & (rng <= target_limits[1])
+
+        rng = rng[index]
+        data = data[:, index]
+
+    min_az = np.nanmin(az)
+    max_az = np.nanmax(az)
+    min_el = np.nanmin(el)
+    max_el = np.nanmax(el)
 
     if delta_x is None:
         delta_x = max_az - min_az
@@ -99,9 +125,12 @@ def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=No
 
     # Get range closest to target_range
     if target_range is None:
-        target_index = 0
-    else:
-        target_index = np.argmin(np.abs(np.array(rng) - target_range))
+        index = np.nanargmax(data)
+        index = np.unravel_index(index, data.shape)
+        target_range = rng[index[1]]
+
+    target_index = np.argmin(np.abs(np.array(rng) - target_range))
+    target_range = rng[target_index]
 
     data = data[:, target_index]
 
@@ -112,20 +141,35 @@ def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=No
     # Grid up the data for plotting
     grid = griddata((az, el), data, (xi, yi), method='linear')
     max_ind = np.unravel_index(np.nanargmax(grid), grid.shape)
-    diff = np.diff(grid[:, max_ind[0]], n=1)
-    idx = (diff == np.nanmin(diff))
-    diff_index = np.where(idx)[0][-1]
-    maxstr = 'Max: ' + str(round(grid[max_ind], 2))
-    minstr = 'Min: ' + str(round(np.nanmin(grid), 2))
-    azstr = 'Az (max): ' + str(round(xi[max_ind], 1))
-    elstr = 'El (max): ' + str(round(yi[max_ind], 1))
-    eltopstr = 'El (top): ' + str(round(yi[diff_index, max_ind[1]], 1))
+    try:
+        diff = copy.deepcopy(grid[slice(max_ind[0] + 1, grid.shape[1]), max_ind[1]])
+        diff[diff < -20] = np.nan
+        top_index = np.nanargmin(np.diff(diff))
+    except ValueError:
+        diff = grid[slice(max_ind[0] + 1, grid.shape[1]), max_ind[1]]
+        top_index = np.nanargmin(np.diff(diff))
+
+    top_index += max_ind[0]
+    top_index = (top_index, max_ind[1])
+
+    data_max = grid[max_ind]
+    data_min = np.nanmin(grid)
+    az_max = xi[max_ind]
+    el_max = yi[max_ind]
+    el_top = yi[top_index]
+
+    maxstr = 'Max: ' + str(round(data_max, 2))
+    minstr = 'Min: ' + str(round(data_min, 2))
+    azstr = 'Az (max): ' + str(round(az_max, 1))
+    elstr = 'El (max): ' + str(round(el_max, 1))
+    eltopstr = 'El (top): ' + str(round(el_top, 1))
+    rngstr = 'Range: ' + str(round(target_range, 1))
 
     # Plot data using pcolormesh
     if noplot is False:
         pm = ax.pcolormesh(xi[0, :], yi[:, 0], grid, vmin=vmin, vmax=vmax, cmap=cmap, shading='auto')
         ax.plot(xi[max_ind], yi[max_ind], 'w+', ms=10)
-        ax.plot(xi[max_ind], yi[diff_index, max_ind[1]], 'wx', ms=10)
+        ax.plot(xi[max_ind], yi[top_index], 'wx', ms=10)
 
         # Add text information
         ax.text(0.88, 0.9, maxstr, fontsize=8, transform=plt.gcf().transFigure)
@@ -133,26 +177,38 @@ def plot_cr_raster(obj, field='reflectivity', target_range=None, ax=None, fig=No
         ax.text(0.88, 0.84, azstr, fontsize=8, transform=plt.gcf().transFigure)
         ax.text(0.88, 0.81, elstr, fontsize=8, transform=plt.gcf().transFigure)
         ax.text(0.88, 0.78, eltopstr, fontsize=8, transform=plt.gcf().transFigure)
+        ax.text(0.88, 0.75, rngstr, fontsize=8, transform=plt.gcf().transFigure)
 
         if title_flag is True:
             if title is None:
                 time_str = pd.to_datetime(str(obj['time'].values[0]))
-                title = ' '.join(['Corner Reflector', field.title(), time_str.strftime('%m/%d/%Y %H:%M:%S')])
+                title = ' '.join(['Corner Reflector', field.title(),
+                                  time_str.strftime('%Y-%m-%d %H:%M:%S')])
             ax.set_title(title)
 
         if axislabels_flag is True:
+            try:
+                az_units = obj[az_field].attrs['units']
+                el_units = obj[el_field].attrs['units']
+            except KeyError:
+                az_units = 'degree'
+                el_units = 'degree'
+
             if axislabels[0] is None:
-                axislabels[0] = 'Azimuth (deg)'
+                axislabels[0] = f"Azimuth ({az_units})"
+
             if axislabels[1] is None:
-                axislabels[1] = 'Elevation (deg)'
+                axislabels[1] = f"Elevation ({el_units})"
+
             ax.set_xlabel(axislabels[0])
             ax.set_ylabel(axislabels[1])
 
         if colorbar_flag:
             if colorbar_label is None:
                 colorbar_label = field.title() + ' (' + obj[field].attrs['units'] + ')'
-            fig.colorbar(mappable=pm, label=colorbar_label, orientation=colorbar_orient,
-                         ax=ax)
 
-    return {'max': grid[max_ind], 'min': np.nanmin(grid), 'az_max': xi[max_ind],
-            'el_max': yi[max_ind], 'el_top': yi[diff_index, max_ind[1]]}
+            fig.colorbar(mappable=pm, label=colorbar_label,
+                         orientation=colorbar_orient, ax=ax)
+
+    return {'max': data_max, 'min': data_min, 'az_max': az_max,
+            'el_max': el_max, 'el_top': el_top, 'range': target_range}
