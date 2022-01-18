0ff26ab876efbcf4917673166747bc96fd9097ec
AdamTheisen <atheisen@anl.gov>
2022-01-18 23:34:24 +0000

    Deploying to gh-pages from  @ f0b826dfc9f4c2f823eaf32f703e8275885bf1af 🚀

--- build/html/_static/documentation_options.js
+++ build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g16007ec',
+    VERSION: '0.post1+gf0b826d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
