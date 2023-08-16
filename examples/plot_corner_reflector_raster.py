e93f45f97a96ffa605928e2b930d98fc2b4ae214
AdamTheisen <atheisen@anl.gov>
2023-08-16 11:46:38 -0500

    ENH: Updating workflow and examples to have ds instead of obj

--- examples/plot_corner_reflector_raster.py
+++ examples/plot_corner_reflector_raster.py
@@ -14,10 +14,10 @@ from act.io.armfiles import read_netcdf
 import matplotlib.pyplot as plt
 
 # Read in sample data using ACT
-obj = read_netcdf(radtraq.tests.sample_files.EXAMPLE_RASTER)
+ds = read_netcdf(radtraq.tests.sample_files.EXAMPLE_RASTER)
 
 # Process and plot raster file
-data = radtraq.plotting.corner_reflector.plot_cr_raster(obj, target_range=478.,
+data = radtraq.plotting.corner_reflector.plot_cr_raster(ds, target_range=478.,
                                                         el_limits=[-0.5, 2.5], noplot=False)
 plt.show()
-obj.close()
+ds.close()
--- examples/plot_self_consistency.py
+++ examples/plot_self_consistency.py
@@ -11,7 +11,7 @@ from act.io.armfiles import read_netcdf
 import matplotlib.pyplot as plt
 
 # Read in example data
-obj = read_netcdf(radtraq.tests.sample_files.EXAMPLE_CSAPR)
+ds = read_netcdf(radtraq.tests.sample_files.EXAMPLE_CSAPR)
 
 # Set thresholds of RhoHv > 0.99
 thresh = {'copol_correlation_coeff': 0.99}
@@ -23,5 +23,5 @@ var_dict = {'differential_reflectivity': {'variable': 'reflectivity', 'bin_width
             'mean_doppler_velocity': {'variable': 'reflectivity', 'bin_width': [1, 0.5]}}
 
 # Call RadTraQ function
-display = radtraq.plotting.plot_self_consistency(obj, variables=var_dict, thresh=thresh)
+display = radtraq.plotting.plot_self_consistency(ds, variables=var_dict, thresh=thresh)
 plt.show()
