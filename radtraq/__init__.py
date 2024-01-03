4d2ee697f04d4cfac4d50825faf6639d163ab765
akrherz <akrherz@iastate.edu>
2024-01-03 13:16:29 -0600

    mnt: update versioneer vendor

--- radtraq/__init__.py
+++ radtraq/__init__.py
@@ -7,3 +7,6 @@ from ._version import get_versions
 # Version for source builds
 vdict = get_versions()
 __version__ = vdict["version"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
